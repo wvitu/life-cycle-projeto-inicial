ab079edde46ccfb837230bbfa6d8b904c2c9adf0
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-04-26 15:14:27 -0300

    @Input(): comunicação de pai para filho
    Criamos uma propriedade no AppComponent que armazene essa informação. Por isso, pressionaremos "Enter" logo abaixo do array de lista de compras e criaremos a propriedade ItemParaSerEditado. Ela começará vazia e será do tipo Item.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ import { ListaDeCompraService } from './service/lista-de-compra.service';
 export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
   listaDeCompra! : Array<Item>
+  itemParaSerEditado! : Item;
+
   constructor(private listaService:
     ListaDeCompraService) { }
 
@@ -18,6 +20,6 @@ export class AppComponent implements OnInit {
     }
 
     editarItem(item: Item){
-      console.log(item);
+      this.itemParaSerEditado = item;
     }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,15 @@
 import { ListaDeCompraService } from './../../service/lista-de-compra.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { log } from 'console';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item;
   valorItem! : string;
   constructor(private listaService: ListaDeCompraService) { }
 
@@ -20,5 +22,11 @@ export class InputComponent implements OnInit {
 
   limparCampo(){
     this.valorItem = '';
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    if (!changes['itemQueVaiSerEditado'].firstChange){
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
   }
 }
